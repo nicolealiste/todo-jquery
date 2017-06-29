0e6e68556b495d897af79a384b43dc1f4c8d132a
Nicole Aliste <nicole.alistecastillo@gmail.com>
2017-06-28 21:52:49 -0400

    seagrega boton remove

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,16 @@ $(document).ready(function(){
 	$("button").click(function(e){
 		e.preventDefault();
 		var task=$("#tarea").val();
+		var lastSibling = $('#todo-list > .todo-wrap:last-of-type > input').attr('id');
+  		var newId = Number(lastSibling) + 1;
 		if(task == ""){
 			alert("Debes anotar una tarea");
 		}else{
-			$("#items").append('<div><span class="left-align">' + task + '</span></div>');
+			$("#items").append('<div><span class="editing todo-wrap"><input type="checkbox" id="'+newId+'"/><label for="'+newId+'" class="todo"><i class="fa fa-check"></i>' + task + '</label></span></div>');
+
 		}
 	})
 })
+
+      
+  
