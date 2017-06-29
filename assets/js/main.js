cfc7bdccfda2719855ff742223a9a9631bc35cc4
Nicole Aliste <nicole.alistecastillo@gmail.com>
2017-06-29 10:59:24 -0400

    Se avanza en style.css

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
 		if(localStorage.task == ""){
 			alert("Debes anotar una tarea");
 		}else{
-			$("#items").append('<div><input type="checkbox" id="'+newId+'"/><label for="'+newId+'" class="todo">' + localStorage.task + '</label><span class="delete-item" title="remove"><button>x</button></span></div>');
+			$("#items").append('<div><input type="checkbox" id="'+newId+'"/><label for="'+newId+'" class="todo">' + localStorage.task + '</label><span class="delete-item" title="remove"><i class="fa fa-trash-o" aria-hidden="true"></i></span></div>');
 			$("#tarea").val("");
 		};
 		$('.delete-item').click(function(){
@@ -34,7 +34,7 @@ $(document).ready(function(){
 	$("#1").click(function(e){
 		e.preventDefault();
 		$(".1").remove();
-		$("#cItems").append('<span class="todo-wrap"><input type="checkbox" id="1" checked/><label for="1" class="todo">Ir de compras</label><span class="delete-item" title="remove"><button>x</button></span></span>');
+		$("#cItems").append('<span class="todo-wrap"><input type="checkbox" id="1" checked/><label for="1" class="todo">Ir de compras</label><span class="delete-item" title="remove"><i class="fa fa-trash-o" aria-hidden="true"></i></span></span>');
 		 
 	});
 	$('.delete-item').click(function(){
