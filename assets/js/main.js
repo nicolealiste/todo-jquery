c16f66be7211020105d84391d0d67c82569f2b66
Nicole Aliste <nicole.alistecastillo@gmail.com>
2017-06-28 22:44:06 -0400

    Se toma un receso

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,29 @@
 $(document).ready(function(){
-	$("button").click(function(e){
-		e.preventDefault();
+	$.fn.enterKey = function (fnc) {
+	    return this.each(function () {
+	        $(this).keypress(function (ev) {
+	            var keycode = (ev.keyCode ? ev.keyCode : ev.which);
+	            if (keycode == '13') {
+	                fnc.call(this, ev);
+	            }
+	        })
+	    })
+	};
+	$("#tarea").enterKey(function(e){
+    	$(this).trigger('enterEvent');
+  		e.preventDefault();
 		var task=$("#tarea").val();
 		var lastSibling = $('#todo-list > .todo-wrap:last-of-type > input').attr('id');
   		var newId = Number(lastSibling) + 1;
 		if(task == ""){
 			alert("Debes anotar una tarea");
 		}else{
-			$("#items").append('<div><span class="editing todo-wrap"><input type="checkbox" id="'+newId+'"/><label for="'+newId+'" class="todo"><i class="fa fa-check"></i>' + task + '</label></span></div>');
+			$("#items").append('<div><span class="editing todo-wrap"><input type="checkbox" id="'+newId+'"/><label for="'+newId+'" class="todo">' + task + '</label><span class="delete-item" title="remove"><button>x</button></span></span></div>');
 
 		}
-	})
+	});
 })
+
 
       
   
