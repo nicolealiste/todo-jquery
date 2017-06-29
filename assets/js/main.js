02f3507bcb213ed4721004a1e9c66846625731bd
Nicole Aliste <nicole.alistecastillo@gmail.com>
2017-06-29 01:56:11 -0400

    Se realiza cambio de lista con tarea fija

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
 	$("#tarea").enterKey(function(e){
     	$(this).trigger('enterEvent');
   		e.preventDefault();
-		localStorage.task=document.getElementById("tarea").value;;
+		localStorage.task=document.getElementById("tarea").value;
 		var lastSibling = $('#todo-list > .todo-wrap:last-of-type > input').attr('id');
   		var newId = Number(lastSibling) + 1;
 		if(localStorage.task == ""){
@@ -20,9 +20,15 @@ $(document).ready(function(){
 		}else{
 			$("#items").append('<div><span class="editing todo-wrap"><input type="checkbox" id="'+newId+'"/><label for="'+newId+'" class="todo">' + localStorage.task + '</label><span class="delete-item" title="remove"><button>x</button></span></span></div>');
 
-		}
+		};
+		
 	});
-	var codes={"1":"#cItems"};
+	$("#1").click(function(e){
+		e.preventDefault();
+		$(".1").remove();
+		$("#cItems").append('<span class="todo-wrap"><input type="checkbox" id="1" checked/><label for="1" class="todo">Ir de compras</label><span class="delete-item" title="remove"><button>x</button></span></span>');
+		 
+	});
 	$('.delete-item').click(function(){
 	  var parentItem = $(this).parent();
 	  $(parentItem).remove(); 
